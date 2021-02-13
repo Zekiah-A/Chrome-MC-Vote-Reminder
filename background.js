c738bdb2ff55876921e874e1472ed6794278bdb2
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2021-02-13 10:57:11 +0000

    Update #2 - Notification groundwork

--- background.js
+++ background.js
@@ -1,8 +1,23 @@
 //This script runs 24/7 IN THE BACKGROUND - Should be used for alarms and notifications
 
-chrome.alarms.onAlarm.addListener(function(alarm) {
-	console.log("Notification Alarm fired", alarm)
-	notification = webkitNotifications.createNotification(
-		'images/drugdealertux', alarm.name, JSON.stringify());
-	notification.show();
+var notificationSettings  = {
+	type: "basic",
+	title: "SuomiCraftPEVote - Reminder", 
+	message: "Hey, you haven't voted for the server today. Please click here to support the server", 
+	iconUrl: "images/scpe_32.png"
+};
+
+function tick(){
+	var d = new Date();
+	var dateIndicator = document.getElementById('dateIndicator');
+	dateIndicator.innerHTML = d
+	console.log("Current date is:", d)
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	notification(); //maybe time var
 });
+
+function notification(){
+	chrome.notifications.create(notificationSettings)
+}
